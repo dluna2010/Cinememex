3156d9c5f5234a08fea95218a738de617a790348
Diego Luna <diego.luna@iteso.mx>
2023-11-16 11:54:54 -0600

    Creación de ruta AdminMovie

--- app/routes/movie.js
+++ app/routes/movie.js
@@ -4,62 +4,62 @@ const ShoppingCart = require('../controllers/shopping_cart');
 
 const router = express.Router();
 
-// Obtener todos los productos
+// Obtener todos los películas
 router.get('/', (req, res) => {
     try {
-        const products = dataHandler.getProducts();
-        res.json(products);
+        const movies = dataHandler.getMovies();
+        res.json(movies);
     } catch (error) {
-        res.status(500).json({ message: "Error fetching products", error: error.message });
+        res.status(500).json({ message: "Error fetching movies", error: error.message });
     }
 });
 
 // Crear una instancia de ShoppingCart
 const cart = new ShoppingCart();
 
-// Agregar productos al carrito (por IDs)
+// Agregar películas al carrito (por IDs)
 router.post('/cart', (req, res) => {
-    const productIds = req.body;
-    if (!Array.isArray(productIds)) {
-        res.status(400).json({ message: "Body should be an array of product IDs" });
+    const movieIds = req.body;
+    if (!Array.isArray(movieIds)) {
+        res.status(400).json({ message: "Body should be an array of movie IDs" });
         return;
     }
     try {
-        const products = productIds.map(id => dataHandler.getProductById(id)).filter(product => product);
-        if (products.length !== productIds.length) {
-            res.status(404).json({ message: "Some products were not found" });
+        const movies = movieIds.map(id => dataHandler.getMovieById(id)).filter(movie => movie);
+        if (movies.length !== movieIds.length) {
+            res.status(404).json({ message: "Some movies were not found" });
             return;
         }
 
-        // Añadir productos al carrito de compras
-        products.forEach(product => cart.addItem(product, 1));  // Uno como cantidad default
+        // Añadir películas al carrito de compras
+        movies.forEach(movie => cart.addItem(movie, 1));  // Uno como cantidad default
 
         // Regresar el contenido del carrito
         res.json(cart.getAllItems());
     } catch (error) {
-        res.status(500).json({ message: "Error fetching products for cart", error: error.message });
+        res.status(500).json({ message: "Error fetching movies for cart", error: error.message });
     }
 });
 
-// Obtener producto por ID
+// Obtener película por ID
 router.get('/:id', (req, res) => {
     try {
-        const product = dataHandler.getProductById(req.params.id);
-        if (!product) {
-            res.status(404).json({ message: "Product not found" });
+        const movie = dataHandler.getMovieById(req.params.id);
+        if (!movie) {
+            res.status(404).json({ message: "Movie not found" });
             return;
         }
-        res.json(product);
+        res.json(movie);
     } catch (error) {
-        res.status(500).json({ message: "Error fetching product", error: error.message });
+        res.status(500).json({ message: "Error fetching movie", error: error.message });
     }
 });
 
-// Buscar producto por nombre o categoría
+// Buscar película por nombre o categoría
 router.get('/search/:query', (req, res) => {
     const query = req.params.query;
-    const products = dataHandler.findProduct(query);
-    res.json(products);
+    const movies = dataHandler.findMovie(query);
+    res.json(movies);
 });
 
 module.exports = router;
