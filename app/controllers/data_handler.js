72249b4ccadbcd0642cd678bfde7bbbde6532bf4
Diego Luna <diego.luna@iteso.mx>
2023-11-16 10:36:44 -0600

    Comienzo de back-end

---
+++ app/controllers/data_handler.js
@@ -0,0 +1,126 @@
+const fs = require('fs');
+const path = require("path");
+
+const { generateUUID } = require("./util");
+
+const moviesFilePath = path.join(__dirname, "..", "data", "movies.json");
+
+// LEER películas del json
+function readMoviesFromFile() {
+    try {
+        const data = fs.readFileSync(moviesFilePath, 'utf8');
+        //console.table(data);
+        return JSON.parse(data);
+    } catch (error) {
+        console.error('Error reading the movies from file:', error);
+        return [];
+    }
+}
+
+// ESCRIBIR películas en el json
+function writeMoviesToFile(movies) {
+    try {
+        fs.writeFileSync(moviesFilePath, JSON.stringify(movies, null, 2), 'utf8');
+    } catch (error) {
+        console.error('Error writing movies to file:', error);
+    }
+}
+
+// Obtener todos los películas
+function getMovies() {
+    let data = readMoviesFromFile();
+    return data;
+}
+
+// Obtener un película por ID
+function getMovieById(uuid) {
+    const movies = readMoviesFromFile();
+    return movies.find(movie => movie.uuid === uuid);
+}
+
+// Crear un nuevo película
+function createMovie(newMovie) {
+    const movies = readMoviesFromFile();
+    newMovie.uuid = generateUUID();
+    movies.push(newMovie);
+    writeMoviesToFile(movies);
+    return newMovie;
+}
+
+// Actualizar un película
+function updateMovie(uuid, updatedMovie) {
+    const movies = readMoviesFromFile();
+    const index = movies.findIndex(movie => movie.uuid === uuid);
+    if (index !== -1) {
+        movies[index] = { ...movies[index], ...updatedMovie };
+        writeMoviesToFile(movies);
+        return movies[index];
+    }
+
+    return null;
+}
+
+// Borrar un película
+function deleteMovie(uuid) {
+    const movies = readMoviesFromFile();
+    const index = movies.findIndex(movie => movie.uuid === uuid);
+    if (index !== -1) {
+        const deletedMovie = movies.splice(index, 1)[0];
+        writeMoviesToFile(movies);
+        return deletedMovie;
+    }
+    return null;
+}
+
+// Encontrar un película mediante una consulta
+// Método adicional para buscar películas por categoría y/o nombre
+function findMovie(query) {
+    if (!query) return [];
+
+    let [category, titulo] = query.split(":").map(str => str.trim());
+
+    if (category === "") {
+        // La búsqueda es por título
+        category = undefined;
+        titulo = titulo.toLowerCase();
+    } else if (titulo === "" || titulo === undefined) {
+        // La búsqueda es por categoría
+        titulo = undefined;
+        category = category.toLowerCase();
+    } else {
+        // La búsqueda es por categoría y título
+        category = category.toLowerCase();
+        titulo = titulo.toLowerCase();
+    }
+
+    const movies = readMoviesFromFile();
+
+    // Agregamos que no discrimine mayúsculas y minúsculas
+    return movies.filter(movie => {
+        const movieCategory = movie.category ? movie.category.toLowerCase() : "";
+        const movieTitulo = movie.titulo ? movie.titulo.toLowerCase() : "";
+
+        let matchCategory = true;
+        let matchTitulo = true;
+
+        if (category) {
+            matchCategory = movieCategory.includes(category);
+        }
+
+        if (titulo) {
+            matchTitulo = movieTitulo.includes(titulo);
+        }
+
+        return matchCategory && matchTitulo;
+    });
+}
+
+module.exports = {
+    getMovies,
+    getMovieById,
+    writeMoviesToFile,
+    createMovie,
+    updateMovie,
+    deleteMovie,
+    findMovie
+};
---
+++ app/controllers/movie.js
@@ -0,0 +1,130 @@
+const { generateUUID } = require("./util");
+
+//Creación de la clase de excepción del película
+class MovieException {
+    constructor(errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
+
+//Creación de la clase película
+class Movie {
+    constructor(titulo, sinopsis, posterUrl, genero, duration, reparto, trailerIframe) {
+        this._uuid = generateUUID(); // Utilizamos el generate UUID
+        this.titulo = titulo;
+        this.sinopsis = sinopsis;
+        this.posterUrl = posterUrl;
+        this.genero = genero;
+        this.duration = duration;
+        this.reparto = reparto;
+        this.trailerIframe = trailerIframe;
+    }
+
+    // Getters y Setters con validación y lanzamiento de excepciones.
+    // Setter y Getter para uuid
+    set uuid(value) {
+        throw new MovieException("El UUID es auto-generado");
+    }
+
+    get uuid() {
+        return this._uuid;
+    }
+
+    // Setter y Getter para título
+    set titulo(value) {
+        if (!value.trim()) throw new MovieException("El título no puede estar vacío");
+        this._titulo = value;
+    }
+
+    get titulo() {
+        return this._titulo;
+    }
+
+    // Setter y Getter para descriçión
+    set sinopsis(value) {
+        if (!value.trim()) throw new MovieException("La sinopsis no puede estar vacía");
+        this._sinopsis = value;
+    }
+
+    get sinopsis() {
+        return this._sinopsis;
+    }
+
+    // Setter y Getter para posterUrl
+    set posterUrl(value) {
+        if (!value.trim()) throw new MovieException("El URL del poster no puede estar vacío");
+        this._posterUrl = value;
+    }
+
+    get posterUrl() {
+        return this._posterUrl;
+    }
+
+    // Setter y Getter para genero
+    set genero(value) {
+        if (!value.trim()) throw new MovieException("El género no puede estar vacío");
+        this._genero = value;
+    }
+
+    get genero() {
+        return this._genero;
+    }
+
+    // Setter y Getter para reparto
+    set reparto(value) {
+        if (!value.trim()) throw new MovieException("El reparto no puede estar vacío");
+        this._reparto = value;
+    }
+
+    get reparto() {
+        return this._reparto;
+    }
+
+    // Setter y Getter para duration
+    set duration(value) {
+        if (value < 0) throw new MovieException("La duración no puede ser negativa");
+        this._duration = value;
+    }
+
+    get duration() {
+        return this._duration;
+    }
+
+    // Setter y Getter para trailerIframe
+    set trailerIframe(value) {
+        if (!value.trim()) throw new MovieException("El iFrame del trailer no puede estar vacío");
+        this._trailerIframe = value;
+    }
+
+    get trailerIframe() {
+        return this._trailerIframe;
+    }
+
+    // Métodos estáticos
+    //Crear producto desde String JSON
+    static createFromJson(jsonValue) {
+        let obj = JSON.parse(jsonValue);
+        return this.createFromObject(obj);
+    }
+
+    //Crear producto desde objeto
+    static createFromObject(obj) {
+        let cleanObj = this.cleanObject(obj);
+        return new Product(cleanObj.uuid, cleanObj.titulo, cleanObj.sinopsis, cleanObj.posterUrl, cleanObj.genero, cleanObj.reparto, cleanObj.duration, cleanObj.trailerIframe);
+    }
+
+    //Limpiar el objeto si tiene cosas extras no pertenecientes a Movie
+    static cleanObject(obj) {
+        let cleanObj = {};
+        // Aquí se especifican solo las propiedades que pertenecen a la clase Movie
+        const validKeys = ["uuid", "titulo", "sinopsis", "posterUrl", "genero", "reparto", "duration", "trailerIframe"];
+        for (let key of validKeys) {
+            if (obj.hasOwnProperty(key)) {
+                cleanObj[key] = obj[key];
+            }
+        }
+        return cleanObj;
+    }
+}
+
+module.exports = Product;
