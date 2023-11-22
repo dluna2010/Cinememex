56ab4e825222f25bc8820cca71ea25d6d4197ab5
Diego Luna <diego.luna@iteso.mx>
2023-11-22 14:51:03 -0600

    Creando db.js

---
+++ app/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const connectionString = 'mongodb+srv://admin:admin@cluster0.necdoj1.mongodb.net/';
+let db = mongoose.connection;
+
+db.on("Connecting", () => {
+    console.log("Conectando...");
+    console.log(mongoose.connection.readyState); //State 2: Connecting
+})
+
+db.on("Connected", () => {
+    console.log("!Conectado exitosamente!");
+    console.log(mongoose.connection.readyState); //State 2: Connecting
+})
+
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+
+module.exports = mongoose;
