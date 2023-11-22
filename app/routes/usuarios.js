5d8fc936a732fe8b807af314948ab46e982800d1
SebasCarrillo1 <sebcarrillo1@gmail.com>
2023-11-21 21:19:50 -0600

    Usuarios

---
+++ app/routes/usuarios.js
@@ -0,0 +1,52 @@
+const Usuario = require('../controllers/usuario');
+const express = require('express');
+const dataHandler = require('../controllers/data_handler');
+
+const router = express.Router();
+
+// Middleware de validación
+function validateAdmin(req, res, next) {
+    const authToken = req.headers['x-auth'];
+    if (authToken !== 'admin') {
+        res.status(403).json({ message: "Acceso no autorizado, no se cuenta con privilegios de administrador" });
+        return;
+    }
+    next();
+}
+
+// Hacemos que use en TODAS las rutas :)
+router.use(validateAdmin);
+
+// Ruta para crear un nuevo usuario
+router.post('/', (req, res) => {
+    try {
+        const newUser = new Usuario(req.body.idUsuario, req.body.nombre, req.body.email, req.body.contraseñaEncriptada);
+        const users = dataHandler.getUsers();
+        users.push(newUser);
+        dataHandler.writeUsersToFile(users);
+        res.status(201).json({ message: 'Usuario creado con éxito', user: newUser });
+    } catch (error) {
+        res.status(500).json({ message: 'Error en el servidor', error: error.message });
+    }
+});
+
+// Ruta para iniciar sesión
+router.post('/login', (req, res) => {
+    try {
+        const { email, contraseña } = req.body;
+        const users = dataHandler.getUsers();
+        const userFound = users.find(user => user._email === email);
+
+        if (userFound && userFound._contraseñaEncriptada === contraseña) {
+            res.status(200).json({ message: 'Inicio de sesión exitoso', user: userFound });
+        } else {
+            res.status(401).json({ message: 'Usuario no encontrado o contraseña incorrecta' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Error en el servidor', error: error.message });
+    }
+});
+
+// Otras rutas para usuarios, si son necesarias
+
+module.exports = router;
