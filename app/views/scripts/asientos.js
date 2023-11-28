2741ebb6781b7fd691fcd82208588ae08e7d72c7
Diego Luna <diego.luna@iteso.mx>
2023-11-27 20:01:08 -0600

    Primera parte de asientos.js

---
+++ app/views/scripts/asientos.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const selectedFuncion = JSON.parse(sessionStorage.getItem('funcionSeleccionada'));
+    const cantidadDeBoletos = sessionStorage.getItem('cantidadBoletos');
+    const asientosContainer = document.getElementById('movie-description');
+
+    const seatsDiv = document.createElement('div');
+    seatsDiv.className = 'container my-5';
+    seatsDiv.innerHTML = `
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <img class="movie-image" src="${selectedFuncion.posterUrl}"
+                        alt="${selectedFuncion.peliculaTitulo}">
+                </div>
+                <div class="col-md-8 movie-info">
+                    <h2>Película: ${selectedFuncion.peliculaTitulo}</h2>
+                    <p>Sucursal: ${selectedFuncion.sucursalNombre}</p>
+                    <p>Fecha: 30 de noviembre de 2023</p>
+                    <p>Función: ${selectedFuncion.horaFuncion}</p>
+                    <p>Cantidad de Boletos: ${cantidadDeBoletos} ($75)</p>
+                </div>
+            </div>
+        </div>
+    `;
+
+    asientosContainer.appendChild(seatsDiv);
+});
